515a037cd7b906d2643bd1c6996184274aca99e1
Ashley <ashleyhaggan@gmail.com>
2014-11-28 14:06:25 +0000

    Has function which creates a copy of a board, assigning the address of the parent as the address of the board copied from

--- conway.c
+++ conway.c
@@ -8,6 +8,10 @@
 
 typedef struct cell{
     int alive;
+    int left;
+    int right;
+    int up;
+    int down;
 }cell;
 
 typedef struct version{
@@ -18,11 +22,15 @@ typedef struct version{
 
 version* create_initial_board(void);
 void print_board(version *board);
+void make_move(version *board);
+void copy_cell_array(cell **original, cell **copy);
+void make_child(version *parent, version *child);
 
 int main(void){
     version *board;
     board = create_initial_board();
-    print_board(board);
+    make_move(board);
+    //print_board(board);
 }
 
 version* create_initial_board(void){
@@ -44,7 +52,7 @@ version* create_initial_board(void){
                 
 void print_board(version *board){
     int row, column;
-   
+    //version tmp_board[HEIGHT][WIDTH];
     for(row = 0; row < HEIGHT; row++){
         for(column = 0; column < WIDTH; column++){
             if(board->grid[row][column].alive == YES){
@@ -56,5 +64,42 @@ void print_board(version *board){
         }
         printf("\n");
     }
-}        
+}    
+
+void make_move(version *board){
+    //int row, column;
+    version tmp_board;
+    make_child(board, &tmp_board);
+    print_board(&tmp_board);
+    tmp_board.grid[0][0].alive = YES;
+    print_board(&tmp_board);
+    print_board(board);
+    /*
+    for(row = 0; row < HEIGHT; row++){
+        for(column = 0; column < WIDTH; column++){
+        }
+    }*/
+}
+    
+void make_child(version *parent, version *child){
+    int row, column;
+    for(row = 0; row < HEIGHT; row++){
+        for(column = 0; column < WIDTH; column++){
+            child->grid[row][column].alive = parent->grid[row][column].alive;
+            child->grid[row][column].left = parent->grid[row][column].left;
+            child->grid[row][column].right = parent->grid[row][column].right;
+            child->grid[row][column].up = parent->grid[row][column].up;
+            child->grid[row][column].down = parent->grid[row][column].down;
+        }
+    }
+    child->parent = parent;
+}
+    
+    
+    
+    
+    
+    
+    
+       
 
