790297808e7e6ec21f19cd8f7f6231cc956c61de
Ashley <ashleyhaggan@gmail.com>
2014-11-29 21:04:29 +0000

    Can find a solution, given a row and column. If the row is 2 or less, it gets stuck in an infinate loop.

--- conway.c
+++ conway.c
@@ -21,6 +21,7 @@ typedef struct version{
     cell grid[HEIGHT][WIDTH];
     int target_row;
     int target_column;
+    int found;
     struct version *next;
     struct version *parent;
     //Could I have a dynamically allocated array of child boards, allocated when number of new child boards was calculated
@@ -29,7 +30,7 @@ typedef struct version{
 version* create_initial_board(void);
 void print_board(version *board);
 void print_list(version *board);
-void make_move(version *board);
+version* make_move(version *board);
 void copy_cell_array(cell **original, cell **copy);
 void make_child(version *parent, version *child);
 int check(version *board, int row, int column, type direction);
@@ -38,12 +39,16 @@ int search_board_list(version *board);
 int compare_grid(version *board, version *tmp_board);
 version* get_start_board(version *board);
 void move(version *board, int row, int column, type direction); 
+version* find_target(version *board);
 
 int main(void){
-    version *board;
+    version *board, *solution;
     board = create_initial_board();
-    make_move(board);
-    print_list(board);
+    board->target_row = 5;
+    board->target_column = 1;
+    solution = find_target(board);
+    //print_list(board);
+    print_board(solution);
 }
 
 version* create_initial_board(void){
@@ -124,7 +129,17 @@ void print_list(version *board){
     }
 }
 
-void make_move(version *board){
+version* find_target(version *board){
+    version *tmp_board;
+    tmp_board = board;
+    
+    while(tmp_board->found == NO){
+        tmp_board = make_move(tmp_board);
+    }
+    return tmp_board;
+}
+
+version* make_move(version *board){
     int row, column;
     type direction;
     version *tmp_board;
@@ -138,17 +153,19 @@ void make_move(version *board){
             
                 if(check(board, row, column, direction) == YES){
                     move(tmp_board, row, column, direction);
-                    
-                    if (search_board_list(tmp_board) != YES){
+                    if (search_board_list(tmp_board) == NO){
                         add_to_list(board, tmp_board);
                     }
-                    //print_board(tmp_board);
+                    if(tmp_board->found == YES){
+                        return tmp_board;
+                    }
                     tmp_board = (version*)malloc(sizeof(version)); //When and how do I free these mallocs?
                     make_child(board, tmp_board);
                 }
             }
         }
     }
+    return board->next; //will be the next board of moves to check
     
 }
 void move(version *board, int row, int column, type direction){
@@ -157,21 +174,33 @@ void move(version *board, int row, int column, type direction){
             board->grid[row][column].alive = NO;
             board->grid[row - 1][column].alive = NO;
             board->grid[row - 2][column].alive = YES;
+            if(board->target_row == row - 2 && board->target_column == column){
+                board->found = YES;
+            }
             break;
         case down:
             board->grid[row][column].alive = NO;
             board->grid[row + 1][column].alive = NO;
             board->grid[row + 2][column].alive = YES;
+            if(board->target_row == row + 2 && board->target_column == column){
+                board->found = YES;
+            }
             break;
         case left:
             board->grid[row][column].alive = NO;
             board->grid[row][column - 1].alive = NO;
             board->grid[row][column - 2].alive = YES;
+            if(board->target_row == row && board->target_column == column - 2){
+                board->found = YES;
+            }
             break;
         case right:
             board->grid[row][column].alive = NO;
             board->grid[row][column + 1].alive = NO;
             board->grid[row][column + 2].alive = YES;
+            if(board->target_row == row && board->target_column == column + 2){
+                board->found = YES;
+            }
             break;
     } 
 }
@@ -240,6 +269,7 @@ void make_child(version *parent, version *child){
     }
     child->target_row = parent->target_row;
     child->target_column = parent->target_column;
+    child->found = parent->found;
     child->parent = parent;
 }
     
